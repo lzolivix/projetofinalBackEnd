27c21d20997c19f1d545bdc86bd676995e81b6d5
Luiz <coriba45@gmail.com>
2024-12-11 21:28:31 -0300

    Atualizando o Controller para Utilizar o Helper

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,32 @@
 import { Request, Response } from 'express';
-import { users, User } from '../models/userModel';
+import { UserRepository } from '../repositories/userRepository';
+import { isValidEmail } from '../helpers/validationHelper';
 
-export const getUsers = (req: Request, res: Response): void => {
-  res.json(users);
+const userRepository = new UserRepository();
+
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await userRepository.getAllUsers();
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
 };
 
-export const createUser = (req: Request, res: Response): void => {
-  const newUser: User = {
-    id: users.length + 1,
-    ...req.body
-  };
-  users.push(newUser);
-  res.status(201).json(newUser);
+export const addUser = async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  // Validando o e-mail com o helper
+  if (!isValidEmail(email)) {
+    return res.status(400).json({ error: 'Email inválido' });
+  }
+
+  try {
+    const user = await userRepository.addUser(name, email);
+    res.status(201).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao adicionar usuário' });
+  }
 };
